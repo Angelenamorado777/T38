4396f9ac8cb6c401143af8f0079fc6f1ca8d3a71
Angelenamorado777 <albertoenamorado123@gmail.com>
2026-02-20 23:56:22 -0600

    Añadiendo el app.get

--- PautaExamenI/app.js
+++ PautaExamenI/app.js
@@ -9,11 +9,18 @@ app.use(express.json());
 let libros = [{id: 1, titulo:"Game of thrones", autor:"R. R. George Martin", aniopublicacion:"1996", estado:"disponible"},
 {id: 2, titulo:"El Caballero de los 7 reinos", autor:"R. R. George Martin", aniopublicacion:"2015", estado:"prestado"},
 {id: 3, titulo:"La danza de Dragones", autor:"R. R. George Martin", aniopublicacion:"2011", estado:"reservado"},
-{id: 3, titulo:"Fuego y Sangre", autor:"R. R. George Martin", aniopublicacion:"2018", estado:"daniado"}];
+{id: 4, titulo:"Fuego y Sangre", autor:"R. R. George Martin", aniopublicacion:"2018", estado:"daniado"}];
 
 
-app.get('Libros')
+app.get("/libros", (req,res) => {
+res.json({status:200, message:"Success",data: libros})
+});
 
+
+app.post("/libros", (req,res) => {
+
+
+});
 
 
 app.listen(PORT, ()=>{
